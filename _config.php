de95413dc17e899acdc8b9644f7352492269c6e5
Morven Lewis-Everley <morven@ilateral.co.uk>
2018-09-01 11:57:46 +0100

    Update config

--- _config.php
+++ _config.php
@@ -1,9 +1,10 @@
 <?php
 
-ModelAdmin::add_extension("ImportAdminExtension");
-$remove = Config::inst()->get('ModelAdmin','removelegacyimporters');
-if($remove === "scaffolded"){
-	Config::inst()->update("ModelAdmin", 'model_importers', array());
+use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Core\Config\Config;
+
+$remove = Config::inst()->get(ModelAdmin::class, 'removelegacyimporters');
+
+if ($remove === "scaffolded") {
+    Config::inst()->update(ModelAdmin::class, 'model_importers', []);
 }
-//cache mappings forever
-SS_Cache::set_cache_lifetime('gridfieldimporter', null);
